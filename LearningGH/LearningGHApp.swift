82b1917158a327533d3c7c0114767bbac69800e9
Amelia B <amelia.burns103@gmail.com>
2023-02-08 19:32:04 -0500

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Amelia Atkins on 2/8/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
